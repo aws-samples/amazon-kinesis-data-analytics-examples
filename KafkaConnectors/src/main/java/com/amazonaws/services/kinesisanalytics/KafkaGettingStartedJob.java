f71299fca6532bac9b65a70dd90e98fbaa4725ae
Pat Fletcher <fletpatr@amazon.com>
2020-01-21 23:36:58 +0000

    Update Kafka example to use latest Kafka connectors

--- KafkaConnectors/src/main/java/com/amazonaws/services/kinesisanalytics/KafkaGettingStartedJob.java
+++ KafkaConnectors/src/main/java/com/amazonaws/services/kinesisanalytics/KafkaGettingStartedJob.java
@@ -4,8 +4,9 @@ import com.amazonaws.services.kinesisanalytics.runtime.KinesisAnalyticsRuntime;
 import org.apache.flink.api.common.serialization.SimpleStringSchema;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer010;
-import org.apache.flink.streaming.connectors.kafka.FlinkKafkaProducer010;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaProducer;
+import org.apache.flink.streaming.util.serialization.KeyedSerializationSchema;
 
 import java.io.IOException;
 import java.util.Map;
@@ -16,14 +17,21 @@ public class KafkaGettingStartedJob {
 
 	private static DataStream<String> createKafkaSourceFromApplicationProperties(StreamExecutionEnvironment env) throws IOException {
 		Map<String, Properties> applicationProperties = KinesisAnalyticsRuntime.getApplicationProperties();
-		return env.addSource(new FlinkKafkaConsumer010<>((String) applicationProperties.get("KafkaSource").get("topic"),
+		return env.addSource(new FlinkKafkaConsumer<>((String) applicationProperties.get("KafkaSource").get("topic"),
 				new SimpleStringSchema(), applicationProperties.get("KafkaSource")));
 	}
 
-	private static FlinkKafkaProducer010<String> createKafkaSinkFromApplicationProperties() throws IOException {
+	private static FlinkKafkaProducer<String> createKafkaSinkFromApplicationProperties() throws IOException {
 		Map<String, Properties> applicationProperties = KinesisAnalyticsRuntime.getApplicationProperties();
-		FlinkKafkaProducer010<String> sink = new FlinkKafkaProducer010<>((String) applicationProperties.get("KafkaSink").get("topic"),
-				new SimpleStringSchema(), applicationProperties.get("KafkaSink"));
+		KeyedSerializationSchema<String> serializationSchema =
+				(KeyedSerializationSchema<String>)(new SimpleStringSchema());
+
+		// Configure FlinkProducer for exactly-once semantics
+		FlinkKafkaProducer<String> sink = new FlinkKafkaProducer<>(
+				(String) applicationProperties.get("KafkaSink").get("topic"),
+				serializationSchema,
+				applicationProperties.get("KafkaSink"),
+				FlinkKafkaProducer.Semantic.EXACTLY_ONCE);
 		return sink;
 	}
 
