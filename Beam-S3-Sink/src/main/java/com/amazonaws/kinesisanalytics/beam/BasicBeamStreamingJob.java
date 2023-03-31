32a4365e0cb25b1dc5c2d70c23634822a8f1eec6
psolomin <p.o.solomin@gmail.com>
2023-03-31 18:56:22 +0100

    Port Kinesis -> S3 pipeline to AWS V2 Beam API

--- Beam-S3-Sink/src/main/java/com/amazonaws/kinesisanalytics/beam/BasicBeamStreamingJob.java
+++ Beam-S3-Sink/src/main/java/com/amazonaws/kinesisanalytics/beam/BasicBeamStreamingJob.java
@@ -1,17 +1,15 @@
 package com.amazonaws.kinesisanalytics.beam;
 
-import com.amazonaws.regions.Regions;
-import com.amazonaws.services.kinesis.clientlibrary.lib.worker.InitialPositionInStream;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.avro.generic.GenericRecord;
-import org.apache.beam.sdk.coders.AvroCoder;
+import org.apache.beam.sdk.extensions.avro.coders.AvroCoder;
 import org.apache.beam.sdk.io.FileIO;
 import org.apache.beam.runners.flink.FlinkRunner;
 import org.apache.beam.sdk.Pipeline;
-import org.apache.beam.sdk.io.kinesis.KinesisIO;
-import org.apache.beam.sdk.io.kinesis.KinesisPartitioner;
-import org.apache.beam.sdk.io.kinesis.KinesisRecord;
+import org.apache.beam.sdk.io.aws2.kinesis.KinesisIO;
+import org.apache.beam.sdk.io.aws2.kinesis.KinesisPartitioner;
+import org.apache.beam.sdk.io.aws2.kinesis.KinesisRecord;
 import org.apache.beam.sdk.io.parquet.ParquetIO;
 import org.apache.beam.sdk.options.PipelineOptionsFactory;
 import org.apache.beam.sdk.options.PipelineOptionsValidator;
@@ -22,10 +20,10 @@ import org.apache.beam.sdk.transforms.windowing.*;
 import org.apache.beam.sdk.values.TypeDescriptor;
 import org.apache.commons.lang3.ArrayUtils;
 import org.apache.parquet.hadoop.metadata.CompressionCodecName;
+import org.checkerframework.checker.nullness.qual.NonNull;
 import org.joda.time.Duration;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import samples.trading.avro.TradeEvent;
+import software.amazon.kinesis.common.InitialPositionInStream;
 
 import java.util.Arrays;
 import java.util.Optional;
@@ -50,15 +48,10 @@ public class BasicBeamStreamingJob {
         }
     }
 
-    private static final class SimpleHashPartitioner implements KinesisPartitioner {
+    private static final class SimpleHashPartitioner implements KinesisPartitioner<byte[]> {
         @Override
-        public String getPartitionKey(byte[] value) {
+        @NonNull public String getPartitionKey(byte[] value) {
             return String.valueOf(Arrays.hashCode(value));
-        }
-
-        @Override
-        public String getExplicitHashKey(byte[] value) {
-            return null;
         }
     }
 
@@ -67,10 +60,7 @@ public class BasicBeamStreamingJob {
         String[] kinesisArgs = BasicBeamStreamingJobOptionsParser.argsFromKinesisApplicationProperties(args, BEAM_APPLICATION_PROPERTIES);
         BasicBeamStreamingJobOptions options = PipelineOptionsFactory.fromArgs(ArrayUtils.addAll(args, kinesisArgs)).as(BasicBeamStreamingJobOptions.class);
         options.setRunner(FlinkRunner.class);
-
-        Regions region = Optional
-                .ofNullable(Regions.fromName(options.getAwsRegion()))
-                .orElse(Regions.fromName(Regions.getCurrentRegion().getName()));
+        options.setShutdownSourcesAfterIdleMs(Long.MAX_VALUE);
 
         PipelineOptionsValidator.validate(BasicBeamStreamingJobOptions.class, options);
         Pipeline p = Pipeline.create(options);
@@ -80,7 +70,6 @@ public class BasicBeamStreamingJob {
                 KinesisIO
                         .read()
                         .withStreamName(options.getInputStreamName())
-                        .withAWSClientsProvider(new DefaultCredentialsProviderClientsProvider(region))
                         .withInitialPositionInStream(InitialPositionInStream.LATEST)
         )
         .apply("Deserialize",
