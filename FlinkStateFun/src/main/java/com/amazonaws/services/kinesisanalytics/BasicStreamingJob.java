f6a5c66190efc515907ea7f06555b13fd3693286
Ali Alemi <alialem@amazon.com>
2023-05-01 11:29:29 -0400

    add example for running statefun on KDA

--- FlinkStateFun/src/main/java/com/amazonaws/services/kinesisanalytics/BasicStreamingJob.java
+++ FlinkStateFun/src/main/java/com/amazonaws/services/kinesisanalytics/BasicStreamingJob.java
@@ -1,5 +1,6 @@
 package com.amazonaws.services.kinesisanalytics;
 
+import org.apache.flink.configuration.Configuration;
 import org.apache.flink.statefun.flink.core.StatefulFunctionsConfig;
 import org.apache.flink.statefun.flink.core.StatefulFunctionsJob;
 import org.apache.flink.statefun.flink.core.StatefulFunctionsUniverseProvider;
@@ -13,13 +14,24 @@ import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 public class BasicStreamingJob {
 
     public static void main(String[] args) throws Exception {
-        final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        final Configuration flinkConfiguration = new Configuration();
+        /**
+         Only set below Flink Configurations, when you run outside of Kinesis Analytics
 
+         flinkConfiguration.set(StateBackendOptions.STATE_BACKEND, "rocksdb");
+         flinkConfiguration.set(CheckpointingOptions.INCREMENTAL_CHECKPOINTS, true);
+         flinkConfiguration.set(CheckpointingOptions.CHECKPOINTS_DIRECTORY, "s3://bucket/flink-checkpoints/");
+         flinkConfiguration.set(SavepointConfigOptions.SAVEPOINT_PATH, "s3://bucket/flink-savepoint/");
+         env.setParallelism(8);
+        flinkConfiguration.set(ExecutionCheckpointingOptions.CHECKPOINTING_INTERVAL, Duration.ofSeconds(30));
+        */
+
+        final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment(flinkConfiguration);
         StatefulFunctionsConfig stateFunConfig = StatefulFunctionsConfig.fromEnvironment(env);
 
         stateFunConfig.setProvider((StatefulFunctionsUniverseProvider) (classLoader, statefulFunctionsConfig) -> {
-            Modules modules = Modules.loadFromClassPath();
-            return modules.createStatefulFunctionsUniverse(stateFunConfig);
+                Modules modules = Modules.loadFromClassPath();
+                return modules.createStatefulFunctionsUniverse(statefulFunctionsConfig);
         });
 
         StatefulFunctionsJob.main(env, stateFunConfig);
